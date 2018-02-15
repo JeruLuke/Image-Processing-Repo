d61a56f7d5e329bd30e2f15997dc377ebe94528b
Jeru Luke <jeru92@gmail.com>
2018-02-15 06:02:43 +0000

    script to split validation images
    Script to split a certain percentage of images as validation set from the original training set

---
+++ Code/validation_split.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+import shutil
+import random
+
+ 
+
+src = 'C:/Users/524316/Desktop/Caltech_101/'
+dest = 'C:/Users/524316/Desktop/Caltech_101_validation/'
+
+val_perc = 0.25 
+
+for folder in os.listdir(src): 
+    print (folder)
+    
+    if os.path.isdir(src + folder):
+##        print ('True')
+#        num_of_files = os.listdir(src + folder)
+##        print(len(num_of_files))
+#        num_of_val_imgs = int(0.25 * len(num_of_files)) + 1
+##        print(num_of_val_imgs)
+#        
+#        files = os.listdir(src + folder)
+#        nfiles = []
+#        for j in files:
+#            if j.endswith('.jpg'):
+#                nfiles.append(j)
+#        choices = random.sample(nfiles, num_of_val_imgs) 
+#        for f in choices:
+#            shutil.move(os.path.join(src + folder + '/' + f), os.path.join(dest + folder))
+        
+        
+        
+        
+        if not os.path.exists(dest + folder):
+            os.makedirs(dest + folder)
+            
+            num_of_files = os.listdir(src + folder)
+#        print(len(num_of_files))
+            num_of_val_imgs = int(0.25 * len(num_of_files)) + 1
+    #        print(num_of_val_imgs)
+            
+            files = os.listdir(src + folder)
+            nfiles = []
+            for j in files:
+                if j.endswith('.jpg'):
+                    nfiles.append(j)
+            choices = random.sample(nfiles, num_of_val_imgs) 
+            for f in choices:
+                shutil.move(os.path.join(src + folder + '/' + f), os.path.join(dest + folder))
+            
